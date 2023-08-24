dee0957e8c888c08abf7e4a5d49baacc095fcdef
KPdev94 <139931826+KPdev94@users.noreply.github.com>
2023-08-24 09:11:36 -0400

    Made pen work correctly
    Maybe UI redesign?

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const boardW = document.querySelector('.board-width');
 let size;
 
 const createBoard = (size) => {
+    mainBoard.innerHTML = '';
     for(let j = 0; j < size; j++) {
         const gameSpace = document.createElement('div');
         mainBoard.appendChild(gameSpace);
@@ -20,18 +21,43 @@ const createBoard = (size) => {
             gameSpace.classList.add('gameSpace');   
         }
     })
-    
-    const gameSpace = document.querySelectorAll('gameSapce');
-    gameSpace.forEach((space) => {
-        space.addEventListener('hover', space.style.background = 'blue')
+    colorSelector();
+}
+
+const colorSelector = (fillColor) => {
+    const gameSpace = document.querySelectorAll('.gameSpace');
+    if(fillColor == null) {
+        fillColor = 'black';
+    }
+    gameSpace.forEach(space => {
+        space.addEventListener('mouseover', () => {
+            space.style.backgroundColor = fillColor;
+        })
     })
 }
 
+const blackBtn = document.querySelector('#black');
+blackBtn.addEventListener('click', () => colorSelector("black"));
 
+const redBtn = document.querySelector('#red')
+redBtn.addEventListener('click', () => colorSelector("red"));
+
+const rainbowBtn = document.querySelector('#rainbow')
+rainbowBtn.addEventListener('click', () => colorSelector("rainbow"));
+
+const smallBtn = document.querySelector('#smallBoard');
+smallBtn.addEventListener('click', () => createBoard(16));
+
+const mediumBtn = document.querySelector('#mediumBoard');
+mediumBtn.addEventListener('click', () => createBoard(96));
 
 const btn = document.querySelector('#custom-size');
 btn.addEventListener('click', () => {
     size = prompt('How big to make the grid?');
+    if(size > 100) {
+        alert("Maxium size is 100 x 100!");
+        size = 100;
+    }
     createBoard(size);
 })
 
